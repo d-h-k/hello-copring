322dd3a3cbd6b0e62f2da62e4293d79d8b88ec96
d-h-k <ultraraptor@naver.com>
2022-11-05 22:03:10 +0900

    코프링 mvc

---
+++ src/main/kotlin/io/hello/web/WebController.kt
@@ -0,0 +1,20 @@
+package io.hello.web
+
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class WebController {
+
+    @GetMapping
+    fun index(model: Model):String {
+        return "index"
+    }
+
+
+    @GetMapping("/sign")
+    fun sign(model: Model) : String {
+        return "sign"
+    }
+}
