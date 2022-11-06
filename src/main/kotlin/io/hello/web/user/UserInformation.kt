4650c96d2b3a4166dd2dfa345ccccca61128017b
d-h-k <ultraraptor@naver.com>
2022-11-06 21:54:46 +0900

    코멘트 남기기

--- src/main/kotlin/io/hello/web/user/UserInformation.kt
+++ src/main/kotlin/io/hello/web/user/UserInformation.kt
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor
 import javax.persistence.*
 
 @Entity
-@Table(name = "users")
+@Table(name = "users") //앗 너무 오랫만에 JPA 를 해서 이게 예약어인지 까먹고 있었다 ㅋㅋ
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 class UserInformation {
