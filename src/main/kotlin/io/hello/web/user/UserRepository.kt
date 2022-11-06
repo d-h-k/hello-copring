1941aed502deac5ee1ea5d65e5f1821ac5e40031
d-h-k <ultraraptor@naver.com>
2022-11-06 22:02:56 +0900

    클래스 이름 수정

--- src/main/kotlin/io/hello/web/user/UserRepository.kt
+++ src/main/kotlin/io/hello/web/user/UserRepository.kt
@@ -1,6 +1,5 @@
 package io.hello.web.user
 
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
 interface UserRepository: CrudRepository<UserInformation, Long> {
