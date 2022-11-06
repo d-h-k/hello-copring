156fdead1d4560527ab6686aa7e82575240395e9
d-h-k <ultraraptor@naver.com>
2022-11-06 22:12:27 +0900

    JPA crud api 사용

--- src/main/kotlin/io/hello/web/user/UserRepository.kt
+++ src/main/kotlin/io/hello/web/user/UserRepository.kt
@@ -1,6 +1,11 @@
 package io.hello.web.user
 
+import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.repository.CrudRepository
 
-interface UserRepository: CrudRepository<UserInformation, Long> {
+interface UserRepository: JpaRepository<UserInformation, Long> {
+    //fun findById(id:Long):UserInformation
+
+    fun findByEmail(email:String):UserInformation
+
 }
